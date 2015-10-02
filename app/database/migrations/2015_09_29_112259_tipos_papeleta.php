ebd60e06a5e49b8e7519e35a5f9a8d421eba1d6b
cordonmm <macordonperez@gmail.com>
2015-10-02 12:50:44 +0200

    Initial Commit

---
+++ app/database/migrations/2015_09_29_112259_tipos_papeleta.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class TiposPapeleta extends Migration
+{
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('tipos_papeleta', function ($table) {
+			$table->engine = 'InnoDB';
+			$table->increments('id')->unsigned();
+			$table->string('descripcion');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('tipos_papeleta');
+	}
+
+}
---
+++ app/database/migrations/2015_09_30_062951_recibos.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Recibos extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('recibos', function($table)
+		{
+			$table->engine = 'InnoDB';
+			$table->increments('id')->unsigned();
+			$table->integer('hermano_id')->unsigned();
+			$table->string('concepto');
+			$table->date('fecha_cobro');
+			$table->float('total');
+			$table->foreign('hermano_id')->references('id')->on('hermanos');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('recibos');
+	}
+
+}
